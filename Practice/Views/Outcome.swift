eb7e5b749e88c9e7a067d51086b3ed0b2de44601
Andrii Landiak <andriylandiak@gmail.com>
2021-04-28 13:18:14 +0300

    commit again...

--- Practice/Views/Outcome.swift
+++ Practice/Views/Outcome.swift
@@ -6,45 +6,52 @@
 //
 
 import SwiftUI
+import UIKit
+
+class AddNewOutcome: ObservableObject {
+    @Published var commentary: String = ""
+    @Published var sum: String = ""
+    @Published var date = Date()
+    @Published var category: String = ""
+}
 
 struct Outcome: View {
     
-    @ObservedObject var transactionVM = TransactionListViewModel()
-    
-    @State private var commentary: String = ""
-    @State private var sum: String = ""
-    @State private var date = Date()
+    @Binding var addNewPresented: Bool
+    private let viewModel = AddOutcomeViewModel()
+    @ObservedObject var newData = AddNewOutcome()
     
     var body: some View {
         Form {
             Text("Витрати | Прибуток | Борг")
             HStack {
                 Image(systemName: "bag")
-                TextField("0", text: $sum).keyboardType(.numberPad).multilineTextAlignment(.trailing)
+                TextField("0", text: $newData.sum).keyboardType(.numberPad).multilineTextAlignment(.trailing)
             }
             HStack {
                 Text("Різні категорії")
             }
             HStack {
                 Image(systemName: "calendar")
-                DatePicker("", selection: $date, displayedComponents: .date).position(x: -15, y: 17)
+                DatePicker("", selection: $newData.date, in: ...Date(), displayedComponents: .date).position(x: 70, y: 16).labelsHidden()
             }
             HStack {
                 Image(systemName: "text.bubble")
-                TextField("Коментар", text: $commentary)
+                TextField("Коментар", text: $newData.commentary)
             }
-        }.toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    HStack {
-                        Button("Зберегти") {
-                           transactionVM.addTransaction(sum: -1213, date: Date(), category: "\u{1F6D2}", commentary: "Wow")
-                           refreshData()
-                    }
-                }
+            HStack (spacing: 30) {
+                Button(action: {self.addNew()}) {
+                    Text("Save")
+                    .bold()
+                }.padding()
             }
         }
+        
+        }
+    func addNew() {
+        let transaction23 = TransactionViewModel(id: UUID(), sum: Double(newData.sum) ?? 0, date: newData.date, category: newData.category, commentary: newData.commentary)
+        viewModel.addTransaction(transaction: transaction23)
+        addNewPresented.toggle()
     }
-    func refreshData() {
-        self.transactionVM.fetchAllTransaction()
-    }
+
 }
